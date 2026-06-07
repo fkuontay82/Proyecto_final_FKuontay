1f7e75e910251fedcfe23768071ff3ecffb8bfa1
fkuontay82 <fkuontayp@gmail.com>
2026-06-07 13:12:13 -0500

    Remove unused matplotlib import
    Removed import of matplotlib.pyplot as plt.

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ st.title("Proyecto Final Franklin Kuontay")
 import streamlit as st
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 import seaborn as sns
 import plotly.express as px
 import plotly.graph_objects as go
